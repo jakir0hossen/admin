6ff9868d9eb5151835261a1e01f9d33466d3a1ba
jakir0hossen <jhbjakir@gmail.com>
2025-01-15 01:37:08 +0600

    deshbord

---
+++ Home.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>LU Magazine</title>
+    <script src="https://kit.fontawesome.com/eaa4609b2f.js" crossorigin="anonymous">
+        
+    </script><script src="https://kit.fontawesome.com/eaa4609b2f.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/79d1bfb033.js" crossorigin="anonymous"></script>
+   
+    <link rel="stylesheet" href="./home.css" />
+</head>
+
+<body>
+<nav>
+        <div class="container">
+            <div class="logo">
+                <img src="../images/logo.png" alt="">
+            </div>
+            
+
+            <div class="navbar">
+                <ul>
+                    <li><a href="../Home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="#">LU</a>
+                     <ul>
+                        <li><a href="./blog.php">Blogs</a></li>
+                        <li><a href="./books.php">Book</a></li>
+                        <li><a href="./event.php">Event</a></li>
+                        <li><a href="./#">Lu Club</a>
+                            <ul>
+                                <li><a href="./lucc.php">LUCC</a></li>
+                                <li><a href="./lussc.php">LUSSC</a></li>
+                                <li><a href="./luec.php">LUEC</a></li>
+                                <li><a href="./ieee.php">IEEE</a></li>
+                                <li><a href="./ludc.php">LUDC</a></li>
+                                <li><a href="./lumuna.php">LUMUNA</a></li>
+                                <li><a href="./bncc.php">LU BNCC</a></li>
+                                <li><a href="./lutc.php">LUTC</a></li>
+                            </ul>
+                        </li>
+                     </ul>
+                    </li>
+                    <li><a href="./news.php">News</a></li>
+                    <li><a href="#">Gallery</a>
+                        
+                        <ul>
+                            <li><a href="./photo.php">Photo & Video</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="./contact.php">Contact</a></li>
+                    <li><a href="./logout.php"><i class="fa-solid fa-right-to-bracket"></i>logout</a></li>
+                </ul>
+                
+            </div>
+
+        </div>
+    </nav>
+
+    <header class="showcase">
+        <div class="container">
+            <div class="text-content">
+              
+                <h1>some LU Magazine article</h1>
+                <p>
+                    The university's annual science fair, a showcase of student research and innovation, captivated attendees with projects ranging from sustainable energy solutions to cutting-edge medical advancements. The event highlighted the university's commitment to fostering creativity and academic excellence.
+                </p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <h1 class="editor-h1">Editor Picks</h1>
+            <div class="articles">
+                <a href="./html/article.html" class="card">
+                    <img src="../images/articles/EN1.jpg" alt="" />
+                    <article>
+                        <p class="entertainment-category">Entertainment</p>
+                        <h1>Leading University Social Services Club 
+                        </h1>
+                        <p>
+                        Established in the spring of 2015, Leading University Social Services Club 
+ (LUSSC) is one of the oldest clubs of Leading University and also the only club whose main objective is to offer assistance to the underprivileged and the unfortunate in society. LUSSC believes that it is a moral duty as citizens of Bangladesh, more importantly as human beings, to help out the people in need.It tries its best to contribute and offer a helping hand to these people in whatever way possible.
+
+                        </p>
+                    </article>
+                </a>
+
+                <a href="./html/article.html" class="card">
+                <img src="../images/articles/dr.jpg" alt="" />
+                    <article>
+                        <p class="sports-category">Blog</p>
+                        <h1>Danobir Dr.Syed Ragib Ali</h1>
+                        <p>
+                        I’m thrilled to celebrate the incredible achievements of our University Computer Club. Your dedication to innovation, collaboration, and learning is truly inspiring. Through hackathons, workshops, and groundbreaking projects, you’ve fostered an environment that encourages creativity and problem-solving. Each of you, regardless of background, contributes to shaping the future of technology in meaningful ways. Your collective efforts highlight the power of teamwork and determination. Keep pushing boundaries, exploring new ideas, and making a lasting impact. The future of technology is bright, thanks to your hard work and passion!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="./html/article.html" class="card">
+                    <img src="../images/articles/tech1.jpg" alt="" />
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Top 11 New Technologies in AI: Exploring the Latest Trends
+                        </h1>
+                        <p>
+                        Nowadays, when the technological industry is developing every month, artificial intelligence (AI) stands as the major force that is progressing every minute and revolutionizing the way we live and work. Some difficult tasks that seemed impossible only a few years ago are now becoming an integral part of our reality. While scientists and engineers are arguing whether these rapid changes are good or bad, businesses should stay ahead of the curve in adopting the new technologies in AI in order to survive and grow in today’s constantly changing landscape.
+                        </p>
+                    </article>
+                </a>
+
+                <a href="./html/article.html" class="card">
+                <img src="../images/articles/SP1.jpg" alt="" />
+                    <article>
+                        <p class="sports-category">Sports</p>
+                        <h1>Leading University Sports Club</h1>
+                        <p>
+                        A Sport Club is a registered student organization dedicated to promoting and developing interest in a specific sport or physical activity. It offers students the opportunity to engage in recreational or competitive play while fostering teamwork, discipline, and leadership. Through practices, events, and competitions, Sport Clubs provide a platform for students to enhance their skills, stay active, and connect with others who share similar interests. These clubs encourage a balanced, healthy lifestyle and contribute to building a vibrant campus community focused on physical wellness and athletic growth.
+
+                        </p>
+                    </article>
+                   
+                </a>
+
+                <a href="./html/article.html" class="card">
+                    <img src="../images/articles/event.jpg" alt="" />
+                    <article>
+                        <p class="technology-category">Event</p>
+                        <h1>Upcoming Events</h1>
+                        <p>
+                        The university event was a fantastic gathering that brought students, faculty, and staff together to celebrate academic and extracurricular achievements. From engaging workshops to live performances, attendees had the chance to participate in a variety of activities that showcased the diverse talents within the campus community. The event fostered connections, encouraged collaboration, and promoted school spirit. It highlighted the importance of both intellectual and personal growth, creating an atmosphere of excitement and unity that left everyone feeling motivated and proud to be part of the university.
+
+                        </p>
+                    </article>
+                </a>
+
+                <a href="./html/article.html" class="card">
+                <img src="../images/articles/club.jpg" alt="" />
+                    <article>
+                        <p class="sports-category">Lu Club</p>
+                        <h1>Unlock Your Potential, Join the Club.</h1>
+                        <p>
+                        IEEE LU Student Branch
+                        Leading University Computer Club (LUCC)
+                        CE Family of Leading University (LUCEF)
+                        Leading University Electronics Club (LUEC)
+                        Leading University Social Service Club (LUSSC)
+                        Leading University Cultural Club (LUCC)
+                        Leading University Photographic Society (LUPS)
+                        Leading University Sports Club (LUSC)
+                        Leading University BNCC
+                        LU Rover Scout Group
+                        Leading University Debating Club (LUDC)
+                        Leading University Research Society (LURS)
+                        BANNED COMMUNITY
+                        ORPHEUS
+                        Leading University Model United Nation (LUMUN)
+                        Leading University Tourist Club (LUTC)
+                        </p>
+                    </article>
+                </a>
+
+             
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footerContainer">
+            <div class="socialIcons">
+                <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://x.com/?lang=en"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.google.co.uk/"><i class="fa-brands fa-google-plus"></i></a>
+                <a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a>
+                
+            </div>
+            <div class="footerNav">
+                <ul>
+                    <li><a href="Home.php">Home</a></li>
+                    <li><a href="news.php">News</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="lu.php">LU</a></li>
+                </ul>
+            </div>
+            
+        </div>
+
+        <div class="footerBottom">
+            <p>Copyright &copy;2024; Design by <span class="designer">LuMagazine</span></p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+
+
+
+
+
---
+++ admin/code.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['registerbtn']))
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['confirmpassword'];
+  
+}
+?>
