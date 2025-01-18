b312a176070853324aff2960909c1191e7a289bf
jakir0hossen <jhbjakir@gmail.com>
2025-01-18 22:20:58 +0600

    update data for admin pannel

--- admin/code.php
+++ admin/code.php
@@ -75,6 +75,26 @@ if (isset($_POST['registerbtn'])) {
 
   }
 
+ if(isset($_POST['delete_btn']))
+ {
+    $id = $_POST['delete_id'];
+
+    $query = "DELETE FROM register WHERE id='$id'";
+    $query_run = mysqli_query($connection,$query);
+
+    if($query_run)
+    {
+      $_SESSION['success'] = "Your Data is DELETED";
+      header('Location: register.php');
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is NOT DELETED";
+        header('Location: register.php');
+    }
+ }
+
+
 
 
 
