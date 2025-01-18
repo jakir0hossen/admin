c5ace6c09cba43dbec8ee5c3f56d76707774dc88
jakir0hossen <jhbjakir@gmail.com>
2025-01-18 21:57:09 +0600

    update the admin pannel data

--- admin/code.php
+++ admin/code.php
@@ -51,7 +51,29 @@ if (isset($_POST['registerbtn'])) {
 
 
 
+  if(isset($_POST['updatebtn']))
+  {
+    $id = $_POST['edit_id'];
+    $username = $_POST['edit_username'];
+    $email = $_POST['edit_email'];
+    $password = $_POST['edit_password'];
 
+
+    $query = "UPDATE register SET username='$username',email='$email',password='$password' WHERE id='$id'";
+    $query_run = mysqli_query($connection, $query);
+
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Date is Update";
+        header('Location: register.php');
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Date is NOT Update";
+        header('Location: register.php');
+    }
+
+  }
 
 
 
--- admin/register_edit.php
+++ admin/register_edit.php
@@ -31,22 +31,26 @@ $connection = mysqli_connect("localhost","root","","adminpanel");
     foreach($query_run as $row)
     {
         ?>
+             <form action="code.php" method="POST">
 
+             <input type="hidden" name="edit_id" value="<?php echo $row['id']?>">
+             
             <div class="form-group">
                 <label>Username</label>
-                <input type="text" name="username" value="<?php echo $row['username']?>" class="form-control" placeholder="Enter Username">
+                <input type="text" name="edit_username" value="<?php echo $row['username']?>" class="form-control" placeholder="Enter Username">
             </div>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" name="username" value="<?php echo $row['email']?>" class="form-control" placeholder="Enter Username">
+                <input type="text" name="edit_email" value="<?php echo $row['email']?>" class="form-control" placeholder="Enter Username">
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input type="text" name="username" value="<?php echo $row['password']?>" class="form-control" placeholder="Enter Username">
+                <input type="text" name="edit_password" value="<?php echo $row['password']?>" class="form-control" placeholder="Enter Username">
             </div>
             <a href="register.php" class="btn btn-danger">CANCEL</a>
-            <button class="btn btn-success">Update </button>
-           
+            <button type="submit" name="updatebtn" class="btn btn-success">Update </button>
+
+            </form>
             <?php
     }
 }
